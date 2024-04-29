4e0b42cb76014acb247eb099914f0d0f291f2002
Gabriel Brandão <gabrielbrandao079@gmail.com>
2024-04-28 23:33:24 -0300

    React 1 -  Relogio e Letreiro

--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//relogio e letreiro
+import Relogio from './Relogio';
+import Letreiro from './Letreiro';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+function RelogioContainer() {
+  return (
+      <div className="Relogio">
+        <header className="Relogio-header">
+          <Relogio />
+        </header>
+      </div>
+  );
+ }
+
+ function LetreiroContainer(){
+  return(
+    <div className="Letreiro">
+        <header className="Letreiro-header">
+          <Letreiro />
+        </header>
+    </div>
+  )
+ }
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+    <RelogioContainer />
+    <LetreiroContainer />
+
+ </React.StrictMode>
 );
+
+
+ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
